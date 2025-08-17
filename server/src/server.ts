e551b472060d771f87849cd814d5e2dd131f884f
suvashish26 <shresthasuvashish@gmail.com>
2025-08-17 13:05:36 +0545

    feat: setup Express server with team and match routes, DB sync, and CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,17 @@
 import express from "express";
-// import cors from "cors";
-
+import cors from "cors";
+import teamRoutes from "./routes/team.routes";
+import matchRoutes from "./routes/match.routes";
+import { sequelize } from "./config";
 const app = express();
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
+app.use("/teams", teamRoutes);
+app.use("/matches", matchRoutes);
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+sequelize.sync({ alter: true }).then(() => {
+  console.log("Database synced");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
